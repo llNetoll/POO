c70e4d28e3cb69b98d8e19b5c9d94f2a68d6f539
llNetoll <joaomoraisneto2000@gmail.com>
2025-12-03 08:15:49 -0300

    Delete Q2 directory

--- Q2/ExpressProcessingStrategy.java
+++
@@ -1,7 +0,0 @@
-public class ExpressProcessingStrategy implements IProcessingStrategy {
-    @Override
-    public ProcessingResult process(Order orderType){
-        System.out.printl("processing in express way...");
-        return ("Expressly processed " + orderType)
-    }
-}
--- Q2/StandardProcessingStrategy.java
+++
@@ -1,7 +0,0 @@
-public class StandardProcessingStrategy implements IProcessingStrategy {
-    @Override
-    public ProcessingResult process(Order order){
-        System.out.println("processing in standard way...");
-        return new ProcessingResult("Standardly processed the " + order);
-    }
-}
